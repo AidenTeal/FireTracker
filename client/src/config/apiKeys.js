51a1e450cddada965d59b66e049f6e41999389d8
AidenTeal <112907438+AidenTeal@users.noreply.github.com>
2024-06-26 21:09:44 -0600

    Delete client/src/config/apiKeys.js

--- client/src/config/apiKeys.js
+++
@@ -1,3 +0,0 @@
-export const openWeatherAPI = "79b802ab8eae053dd38454f01f337a4b";
-export const nasaAPI = "52a8f8b9a0b8bffb487ac177f0564d03";
-export const gAPI = "AIzaSyAFP7_0SHvJWTHy_rw0vCGdk8yl4qikfgQ";
